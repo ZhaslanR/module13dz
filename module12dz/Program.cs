edbbcfaa0809cae724adebc40b463c4e61cbf08d
ZhaslanR <zhaslan_12@bk.ru>
2017-12-28 16:42:47 +0600

    Добавьте файлы проекта.

---
+++ module12dz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module12dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int secondMax = 0;
+            List<int> numbers = new List<int>();
+            const int sizeNumbers = 15;
+            const int minValue = 1;
+            const int maxValue = 25;
+            Random rand = new Random();
+
+            for(int i = 0;i<sizeNumbers;i++)
+            {
+                numbers.Add(rand.Next(minValue,maxValue));
+                numbers.Sort();
+            }
+
+            foreach(int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach(int num in numbers)
+            { 
+                if(num > max)
+                {
+                    secondMax = max;
+                    max = num;
+                }
+            }
+
+            Console.WriteLine("Максимальное число : {0}", numbers.Max());
+            Console.WriteLine("Второе после максимального число : {0}", secondMax);
+
+            Console.ReadLine();
+        }
+    }
+}
